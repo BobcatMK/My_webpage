34231b55c058c1fc9d58c75186d30039a7182436
Mateusz <matthew.kilan@gmail.com>
2014-06-27 15:35:22 +0200

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ MyWebpage::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   
   # email enabled in production
-  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"]}
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.cam",
+    address: "smtp.gmail.com",
     port: 587,
     domain: ENV["DOMAIN_NAME"],
     authentication: "plain",
